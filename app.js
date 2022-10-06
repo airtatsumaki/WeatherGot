39be78c7a4b0ea6de98f78de3d69eac6c30463e3
airtatsumaki <naz1mb20@gmail.com>
2022-10-06 10:51:18 +0100

    server setup

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+//import statements using ESM
+// 1) "type": "module", must be added to package.json
+// 2) must have node v14 and above installed
+// 3) may need to delete npm and npm-cache folders in user/appdata/roaming...
+// .. then reinstall global node packages (node, nodemon)
+
 import express, { json } from 'express';
-// const bodyParser = require("body-parser");
 import got from "got";
+import path from 'path';
+import {fileURLToPath} from 'url';
 const app = express();
+app.use(express.static("public"));
+//instead of using body-parser
 app.use(json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+// __dirname fix for ESM.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.get("/", (req,res) => {
   res.sendFile(__dirname + "/index.html");
