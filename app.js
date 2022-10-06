8b181eeaad03e842fc3fc117a9cdbb12e91d3990
airtatsumaki <naz1mb20@gmail.com>
2022-10-06 14:12:56 +0100

    complete

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ import {fileURLToPath} from 'url';
 import * as dotenv from 'dotenv';
 dotenv.config();
 const app = express();
-app.use(express.static("public"));
-//instead of using body-parser
+// app.use(express.static("public"));
+// instead of using body-parser
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 // __dirname fix for ESM.
@@ -24,19 +24,26 @@ app.get("/", (req,res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-//must use async keyword when using got() within a function
+//must use async keyword when using await within a function
 app.post("/", async (req, res) => {
-  // res.send('POST request to homepage');
-  console.log(req.body.city);
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + req.body.city + "&appid=" + process.env.APP_ID;
-  const data = await got(url).json();
-  console.log(data);
-  
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + req.body.city + "&appid=" + process.env.APP_ID + "&units=metric";
+  try {
+    const data = await got(url).json();
+    //needed for cities with special characters.
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.write("<h1>The current weather condition in " + String(data.name) + " is " + parseInt(data.main.temp) + "<sup>o</sup>, " + String(data.weather[0].description).toLowerCase() + "</h1>");
+    res.write("<img src='http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png' style='width: 200px;'/>");
+    res.send();
+  }
+  catch(error){
+    res.write("OPPS! Something went wrong. Error code: " + error.response.statusCode + ". Try again.");
+    res.send();
+  }
 });
 
 app.listen(3000, () => {
   console.log("Server is running!")
-  console.log(process.env.APP_ID);
+  //console.log(process.env.APP_ID);
 });
 
 // weather app api call
